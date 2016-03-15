381e49949c8123f6a34e522d8b8d75db6cc101c5
Brian Beloshapka <bbelos@hotmail.com>
2016-03-15 09:20:55 -0500

    mkvendor.sh: Initial commit of p023

---
+++ cm.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := p023
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/p023/device_p023.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := p023
+PRODUCT_NAME := cm_p023
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := p023
+PRODUCT_MANUFACTURER := asus
